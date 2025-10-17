5d32a0659ee514a97b6d15e5a9142802f7571633
Camel Djoulako <cameldjoulako17@gmail.com>
2025-10-17 04:03:46 +0100

    bind repo with

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,8 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use App\Src\Domain\Intervention\Repositories\TaskRepository;
+use App\Src\Infrastructure\Persistence\Intervention\EloquentTaskRepository;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +13,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(TaskRepository::class, EloquentTaskRepository::class);
     }
 
     /**
